d39eeedaa03712180fde1ba0b1c3241986d21445
tjlaboss <travis@ktc-technology.com>
2016-08-02 12:11:04 -0400

    Created Material instances from FUEL data
    FUEL blocks are defined differently than MATERIAL in Vera. I completed
    the method Case.__get_fuel(fuel) in 'read_xml.py', to instantiate a
    vanilla Material object for each fuel as defined in Vera.
    
    This required the creation of a new module, 'isotopes.py', containing
    the relative atomic weights of Oxygen and of nuclides I expect to
    encounter in Vera decks.

---
+++ isotopes.py
@@ -0,0 +1,32 @@
+# isotopes.py
+#
+# Module containing useful isotopic data
+# NOTE: It would be really useful if a complete table of this were included in the OpenMC Python API
+
+
+# Some relative atomic masses from NIST: http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
+MASS={
+	
+'h-1'   :     1.007825032239,
+'h-2'   :     2.014101778122,
+'h-3'   :     3.016049277924,
+
+'o-16' 	:    15.9949146196, 	 
+'o-17' 	:    16.9991317565,
+'o-18' 	:    17.9991596129,
+'th-230':	230.033134119, 	  
+'th-232':	232.038055821,
+
+'u-233' :	233.039635529,	  
+'u-234' :	234.040952319,
+'u-235' :	235.043930119,
+'u-236' :	236.045568219,	  
+'u-238' :	238.050788420, 
+
+'pu-238':	238.049560119, 	 
+'pu-239': 	239.052163619,	  
+'pu-240':	240.053813819,	  
+'pu-241':	241.056851719,  
+'pu-242':	242.058742820, 
+'pu-244':	244.064205356,
+}
