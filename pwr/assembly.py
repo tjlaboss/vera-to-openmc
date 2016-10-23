13e4ee7964281b205f519575dbb688cbde697d70
tjlaboss <travis@ktc-technology.com>
2016-10-23 16:30:29 -0400

    Moved get_planes to a new file

--- pwr/assembly.py
+++ pwr/assembly.py
@@ -6,7 +6,7 @@
 
 import openmc
 from pwr.nozzle import Nozzle
-#from functions import fill_lattice
+from pwr.functions import *
 from copy import copy
 from math import sqrt
 
@@ -17,8 +17,6 @@ openmc_surface_count	= openmc.AUTO_SURFACE_ID + 1
 openmc_cell_count 		= openmc.AUTO_CELL_ID + 1
 openmc_material_count	= openmc.AUTO_MATERIAL_ID + 1
 openmc_universe_count	= openmc.AUTO_UNIVERSE_ID + 1
-
-
 
 
 
@@ -50,53 +48,6 @@ def counter(count):
 			raise IndexError("Index " + str(count) + " is not SURFACE, CELL, MATERIAL, or UNIVERSE.")
 
 
-def get_plane(surface_list, dim, val, boundary_type = "transmission", name = "", eps = 5):
-	'''Return an instance of openmc.(X/Y/Z)Plane. Check if it exists, within
-	a precision of 'eps'. If so, return it. Otherwise, create it.
-	
-	Inputs:
-		surface_list:	list of instances of openmc.Surface; the list to check for
-						surfaces in. WILL BE MODIFIED. 
-		dim:			str; 'x', 'y', or 'z'
-		val:			float; value for x0, y0, or z0
-		boundary_type:	"transmission", "vacuum", or "reflective".
-						[Default: "transmission"]
-		name:			str; creative name of surface
-						[Default: empty string]
-		eps:			int; number of decimal places after which two planes
-						are considered to be the same.
-						[Default: 5]	'''
-	dim = dim.lower()
-	valid = ("x", "xplane", "y", "yplane", "z", "zplane")
-	assert (dim in valid), "You must specify one of " + str(valid)
-	
-	if dim in ("x", "xplane"):
-		for xplane in surface_list:
-			if isinstance(xplane, openmc.XPlane):
-				if val == round(xplane.x0, eps):
-					return xplane
-		xplane =  openmc.XPlane(counter(SURFACE),
-					boundary_type = boundary_type, x0 = val, name = name)
-		surface_list.append(xplane)
-		return xplane
-	elif dim in ("y", "yplane"):
-		for yplane in surface_list:
-			if isinstance(yplane, openmc.YPlane):
-				if val == round(yplane.y0, eps):
-					return yplane
-		yplane =  openmc.YPlane(counter(SURFACE),
-					boundary_type = boundary_type, y0 = val, name = name)
-		surface_list.append(yplane)
-		return yplane
-	elif dim in ("z", "zplane"):
-		for zplane in surface_list:
-			if isinstance(zplane, openmc.ZPlane):
-				if val == round(zplane.z0, eps):
-					return zplane
-		zplane =  openmc.ZPlane(counter(SURFACE),
-					boundary_type = boundary_type, z0 = val, name = name)
-		surface_list.append(zplane)
-		return zplane
 
 """
 def duplicate(orig):
@@ -425,10 +376,10 @@ class Assembly(object):
 		if self.walls:
 			[min_x, max_x, min_y, max_y] = self.walls
 		else:
-			min_x = get_plane(self.openmc_surfaces, 'x', -half, name = self.name + ' - min_x') 
-			max_x = get_plane(self.openmc_surfaces, 'x', +half, name = self.name + ' - max_x') 
-			min_y = get_plane(self.openmc_surfaces, 'y', -half, name = self.name + ' - min_y') 
-			max_y = get_plane(self.openmc_surfaces, 'y', +half, name = self.name + ' - max_y') 
+			min_x = get_plane(self.openmc_surfaces, counter, 'x', -half, name = self.name + ' - min_x') 
+			max_x = get_plane(self.openmc_surfaces, counter, 'x', +half, name = self.name + ' - max_x') 
+			min_y = get_plane(self.openmc_surfaces, counter, 'y', -half, name = self.name + ' - min_y') 
+			max_y = get_plane(self.openmc_surfaces, counter, 'y', +half, name = self.name + ' - max_y') 
 		
 		self.openmc_surfaces = [min_x, max_x, min_y, max_y]
 		self.wall_region = openmc.Intersection(+min_x & +min_y & -max_x & -max_y)
---
+++ pwr/functions.py
@@ -0,0 +1,54 @@
+# Functions
+#
+# Container for useful functions for the pwr module
+
+
+
+def get_plane(surface_list, counter, dim, val, boundary_type = "transmission", name = "", eps = 5):
+	'''Return an instance of openmc.(X/Y/Z)Plane. Check if it exists, within
+	a precision of 'eps'. If so, return it. Otherwise, create it.
+	
+	Inputs:
+		surface_list:	list of instances of openmc.Surface; the list to check for
+						surfaces in. WILL BE MODIFIED. 
+		counter:		function that keeps track of your surface/cell numbers
+		dim:			str; 'x', 'y', or 'z'
+		val:			float; value for x0, y0, or z0
+		boundary_type:	"transmission", "vacuum", or "reflective".
+						[Default: "transmission"]
+		name:			str; creative name of surface
+						[Default: empty string]
+		eps:			int; number of decimal places after which two planes
+						are considered to be the same.
+						[Default: 5]	'''
+	dim = dim.lower()
+	valid = ("x", "xplane", "y", "yplane", "z", "zplane")
+	assert (dim in valid), "You must specify one of " + str(valid)
+	
+	if dim in ("x", "xplane"):
+		for xplane in surface_list:
+			if isinstance(xplane, openmc.XPlane):
+				if val == round(xplane.x0, eps):
+					return xplane
+		xplane =  openmc.XPlane(counter(SURFACE),
+					boundary_type = boundary_type, x0 = val, name = name)
+		surface_list.append(xplane)
+		return xplane
+	elif dim in ("y", "yplane"):
+		for yplane in surface_list:
+			if isinstance(yplane, openmc.YPlane):
+				if val == round(yplane.y0, eps):
+					return yplane
+		yplane =  openmc.YPlane(counter(SURFACE),
+					boundary_type = boundary_type, y0 = val, name = name)
+		surface_list.append(yplane)
+		return yplane
+	elif dim in ("z", "zplane"):
+		for zplane in surface_list:
+			if isinstance(zplane, openmc.ZPlane):
+				if val == round(zplane.z0, eps):
+					return zplane
+		zplane =  openmc.ZPlane(counter(SURFACE),
+					boundary_type = boundary_type, z0 = val, name = name)
+		surface_list.append(zplane)
+		return zplane
