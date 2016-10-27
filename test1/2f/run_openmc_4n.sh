974f1fe89c4d3e07d416ea266bc034b3c8101f7e
tjlaboss <travis@ktc-technology.com>
2016-10-27 14:43:15 -0400

    Started getting more results

---
+++ test1/2f/run_openmc_4n.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+#
+# partition (queue) 
+#SBATCH -p fast
+#
+# number of nodes 
+#SBATCH -N 1 
+#
+# number of cores 
+#SBATCH -n 4 
+#
+# memory pool for all cores 
+#  comment SBATCH --mem 100 
+#
+# time (D-HH:MM) 
+#  comment SBATCH -t 0-8:00 
+#
+# STDOUT 
+#SBATCH -o slurm.%N.%j.out 
+# STDERR 
+#SBATCH -e slurm.%N.%j.err 
+# notifications for job done & fail 
+#SBATCH --mail-type=END,FAIL 
+# send-to address
+#SBATCH --mail-user=travis.labossiere@gmail.com
+
+
+export OPENMC_CROSS_SECTIONS=$MCNPXS
+openmc
