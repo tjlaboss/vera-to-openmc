2b8f5c8771d09e8cf8c21b950062a0450d4b1cd3
tjlaboss <travis@ktc-technology.com>
2016-12-12 14:37:24 -0500

    Fixes to the Assembly test

--- pwr/assembly.py
+++ pwr/assembly.py
@@ -241,7 +241,7 @@ class Assembly(object):
 			# And then prepare for the next loop around
 			last_s = s
 		
-		# Great, we've done all the layers now!
+		# Now we've done all the lattice layers!
 		# Add the top nozzle if necessary:
 		if self.upper_nozzle:
 			unoz = openmc.Cell(self.counter.add_cell(), "upper nozzle")
@@ -274,31 +274,31 @@ class Assembly(object):
 if __name__ == '__main__':
 	from pwr.mixture import Mixture
 	# Define a global test moderator
-	mod = openmc.Material(1, "mod")
+	c = pwr.Counter()
+	mod = openmc.Material(c.add_material(), "mod")
 	mod.set_density("g/cc", 1.0)
 	mod.add_nuclide("h1", 2.0/3, 'ao')
 	mod.add_nuclide("o16", 1.0/3, 'ao')
 	
 	# Define a simple test material
-	iron = openmc.Material(2, "iron")
+	iron = openmc.Material(c.add_material(), "iron")
 	iron.set_density("g/cc", 7.8)
 	iron.add_element("Fe", 1, 'ao', expand=True)
 	
-	mix1 = Mixture([mod, iron], [0.5,0.5], 33, 'watery iron')
+	mix1 = Mixture([mod, iron], [0.5,0.5], c.add_material(), 'watery iron')
 	assert isinstance(mix1, openmc.Material)	
-	noz1 = pwr.Nozzle(10, 6250, iron, mod, 1, 10)
+	noz1 = pwr.Nozzle(10, 6250, iron, mod, 1, 10, c)
 
 	# Test a pincell
-	cyl0 = openmc.ZCylinder(10, R = 0.300) 
-	cyl1 = openmc.ZCylinder(11, R = 0.333)
-	cyl2 = openmc.ZCylinder(12, R = 0.350)
-	ring0 = openmc.Cell(100, fill = iron, region = -cyl0)
-	ring1 = openmc.Cell(101, fill = mod, region = (-cyl1 & +cyl0) )
-	ring2 = openmc.Cell(102, fill = mix1, region = (-cyl2 & +cyl1) )
-	outer = openmc.Cell(199, fill = mod, region = +cyl2)
-	uni = openmc.Universe(cells = (ring0, ring1, ring2, outer), name = "test pincell")
+	cyl0 = openmc.ZCylinder(c.add_surface(), R = 0.300) 
+	cyl1 = openmc.ZCylinder(c.add_surface(), R = 0.333)
+	cyl2 = openmc.ZCylinder(c.add_surface(), R = 0.350)
+	ring0 = openmc.Cell(c.add_cell(), fill = iron, region = -cyl0)
+	ring1 = openmc.Cell(c.add_cell(), fill = mod, region = (-cyl1 & +cyl0) )
+	ring2 = openmc.Cell(c.add_cell(), fill = mix1, region = (-cyl2 & +cyl1) )
+	outer = openmc.Cell(c.add_cell(), fill = mod, region = +cyl2)
+	uni = openmc.Universe(c.add_universe(), cells = (ring0, ring1, ring2, outer), name = "test pincell")
 	print(uni)
-	gridded = pwr.spacergrid.add_spacer_to(uni, 1.0, 0.10, iron)
+	gridded = pwr.spacergrid.add_spacer_to(uni, 1.0, 0.10, iron, c, [])
 	print(gridded)
-	#print(duplicate(uni))
 
