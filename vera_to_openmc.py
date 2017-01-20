9e910795ae99aa93d16f7994313b5f5281c120e1
tjlaboss <travis@ktc-technology.com>
2017-01-19 21:48:42 -0500

    Programmed normal cases (not bug-free)

--- vera_to_openmc.py
+++ vera_to_openmc.py
@@ -87,6 +87,132 @@ class MC_Case(Case):
 		return xlist, ylist, zlist
 	
 	
+	def get_openmc_baffle2(self):
+		"""
+		Re-imagining the baffle construction.
+		"""
+		
+		"""
+		A simpler method that assumes only outer edges, e.g.,
+		
+		   ___					   _____
+		 _|   |_				  |_ 
+		|       |		not		    |
+		|_     _|				   _|
+		  |___|					  |_____
+		
+		"""
+		baf = self.core.baffle		# instance of objects.Baffle
+		pitch = self.core.pitch		# assembly pitch
+		
+		# Useful distances
+		d0 = pitch/2.0					# dist (from center of asmbly) to edge of asmbly
+		d1 = d0 + baf.gap 				# dist to inside of baffle
+		d2 = d1 + baf.thick				# dist to outside of baffle 
+		d3 = d0 - baf.gap 				# dist to inside of next baffle
+		d4 = d0 - baf.gap - baf.thick	# dist to outside of next baffle 
+		width = self.core.size * self.core.pitch / 2.0	# dist from center of core to center of asmbly
+		
+		cmap = self.core.square_maps("s")
+		n = self.core.size - 1
+		
+		# Unite all individual regions with the Master Region
+		master_region = openmc.Union()
+		
+		
+		# For each row (moving vertically):
+		for j in range(1,n):
+			# For each column (moving horizontally):
+			for i in range(1,n):
+				this = cmap[j][i]
+				if this:
+					# Positions of surfaces
+					x = (i + 0.5)*pitch - width
+					y = width - (j + 0.5)*pitch
+					
+					north = cmap[j-1][i]
+					south = cmap[j+1][i]
+					east  = cmap[j][i+1]
+					west  = cmap[j][i-1]
+					
+					
+					# Left edge
+					if not west:
+						x_left = x - d2
+						x_right = x - d1
+						if north:
+							y_top = y + d3	#d4?
+						else:
+							y_top = y + d2
+						if south:
+							y_bot = y - d3	#d4?
+						else:
+							y_bot = y - d2
+						(left, right), (bot, top) = self.__get_xyz_planes( \
+							x0s = (x_left, x_right), y0s = (y_bot, y_top))[0:2]
+						west_region = (+left & -right & +bot & -top)
+						master_region.nodes.append(west_region)
+					
+					# Right edge
+					if not east:
+						x_left = x + d1
+						x_right = x + d2
+						if north:
+							y_top = y + d3	#d4?
+						else:
+							y_top = y + d2
+						if south:
+							y_bot = y - d3	#d4?
+						else:
+							y_bot = y - d2
+						(left, right), (bot, top) = self.__get_xyz_planes( \
+							x0s = (x_left, x_right), y0s = (y_bot, y_top))[0:2]
+						east_region = (+left & -right & +bot & -top)
+						master_region.nodes.append(east_region)
+					
+					# Top edge
+					if not north:
+						y_bot = y + d1
+						y_top = y + d2
+						if west:
+							x_left = x - d3
+						else:
+							x_left = x - d2
+						if east:
+							x_right = x + d3
+						else:
+							x_right = x + d2
+						(left, right), (bot, top) = self.__get_xyz_planes( \
+							x0s = (x_left, x_right), y0s = (y_bot, y_top))[0:2]
+						north_region = (+left & -right & +bot & -top)
+						master_region.nodes.append(north_region)
+					
+					# Bottom edge
+					if not south:
+						y_bot = y - d2
+						y_top = y - d1
+						if west:
+							x_left = x - d3
+						else:
+							x_left = x - d2
+						if east:
+							x_right = x + d3
+						else:
+							x_right = x + d2
+						(left, right), (bot, top) = self.__get_xyz_planes( \
+							x0s = (x_left, x_right), y0s = (y_bot, y_top))[0:2]
+						south_region = (+left & -right & +bot & -top)
+						master_region.nodes.append(south_region)
+						print("\ny_bot", y_bot, "\ty_top", y_top, "\nx_left", x_left, "\tx_right", x_right)
+		
+		
+		# Set the baffle material, cell, etc.
+		baffle_cell = openmc.Cell(self.__counter(CELL), "Baffle", self.get_openmc_material(baf.mat), master_region)
+		
+		return baffle_cell
+		
+	
+	
 	def get_openmc_baffle(self):
 		'''Generate the surfaces and cells required to model the baffle plates.
 		
@@ -180,8 +306,7 @@ class MC_Case(Case):
 			for i in range(1,n):
 				
 				
-				this = cmap[j][i]
-				if this:
+				if cmap[j][i]:
 					# Positions of surfaces
 					x = (i + 0.5)*pitch - width;	y = width - (j + 0.5)*pitch
 					
