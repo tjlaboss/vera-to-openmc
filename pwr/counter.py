ef928a576988aa0804ec4b88e9fbfd22bf7db16c
tjlaboss <travis@ktc-technology.com>
2017-01-31 18:40:30 -0500

    Removed deprecated Counter.count(TYPE) method

--- pwr/counter.py
+++ pwr/counter.py
@@ -3,7 +3,6 @@
 # Container for the 'Counter' class, which allows the transfer of the 4 essential
 # OpenMC counters back and forth between modules
 
-from pwr.settings import SURFACE, CELL, MATERIAL, UNIVERSE, TALLY
 from openmc import AUTO_SURFACE_ID, AUTO_CELL_ID, AUTO_MATERIAL_ID, AUTO_UNIVERSE_ID, AUTO_TALLY_ID
 
 class Counter(object):
@@ -44,24 +43,7 @@ class Counter(object):
 		rep += "\nUniverse:\t" + str(self.universe)
 		rep += "\nTally:   \t" + str(self.tally)
 		return rep
-	
-	
-	def count(self, TYPE):
-		if TYPE == SURFACE:
-			c = self.add_surface()
-		elif TYPE == CELL:
-			c = self.add_cell()
-		elif TYPE == MATERIAL:
-			c = self.add_material()
-		elif TYPE == UNIVERSE:
-			c = self.add_universe()
-		elif TYPE == TALLY:
-			c = self.add_tally()
-		else:
-			raise TypeError("TYPE must be SURFACE, CELL, MATERIAL, or UNIVERSE")
-		return c
 		
-	
 	def add_surface(self):
 		self.surface += 1
 		return self.surface
