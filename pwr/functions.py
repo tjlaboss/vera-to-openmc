318e7d83aa1d85c673dccd5ef7374989841e22d2
tjlaboss <travis@ktc-technology.com>
2017-01-31 16:05:55 -0500

    Added get_surface() function

--- pwr/functions.py
+++ pwr/functions.py
@@ -8,7 +8,7 @@ import copy
 
 
 def duplicate(orig, counter):
-	'''Copy an OpenMC object, except for a new id 
+	'''Copy an OpenMC object, except for a new id
 
 	Input:
 		orig: 		instance of openmc.(Surface, Cell, Material, or Universe)
@@ -116,5 +116,47 @@ def get_xyz_planes(openmc_surfaces, count, x0s = (), y0s = (), z0s = (), rd = 5)
 	
 	return xlist, ylist, zlist
 
+
+def get_surface(counter, surfdict, dim, coeff, rd = 5):
+	"""Given a single-coefficient Surface class (such as ZPlane, or Cylinder centered at 0,0),
+	look it up in the provided dictionary 'surfdict' if possible. If not, generate it anew,
+	and add it to the dictionary.
+
+	Warning: This assumes you want a surface with the "transmission" boundary condition.
+	If you need a different bc, do not use this function.
+
+	Inputs:
+		:param counter:         instance of pwr.Counter
+		:param surfdict:        dictionary of some type of instance of openmc.Surface, of the format
+		                        {str(coeff):openmc.Surface}; e.g., {str(x0):openmc.XPlane)
+		:param dim:             str; dimension or surface type. Case insensitive.
+								Currently works for ("x"/"xplane", "y"/"yplane", "z"/"zplane", "r"/"cyl"/"zcylinder")
+		:param coeff:           float; Value of the coefficent (such as x0 or R) for the surface type
+		:param rd:              int; number of decimal places to round to. If the coefficient for a surface matches
+								up to 'rd' decimal places, they are considered equal.
+								[Default: 5]
+	Output:
+		:return openmc_surf:    instance of openmc.Surface
+	"""
+	coeff = round(coeff, rd)
+	key = str(coeff)
+	dim = dim.lower()
+	if key in surfdict:
+		return surfdict[key]
+	else:
+		# Generate it
+		if dim in ("x", "xp", "xplane"):
+			openmc_surf = openmc.XPlane(counter.add_surface(), x0 = coeff)
+		elif dim in ("y", "yp", "yplane"):
+			openmc_surf = openmc.YPlane(counter.add_surface(), y0 = coeff)
+		elif dim in ("z", "zp", "zplane"):
+			openmc_surf = openmc.ZPlane(counter.add_surface(), z0 = coeff)
+		elif dim in ("r", "cyl", "cylinder", "zcylinder"):
+			openmc_surf = openmc.ZCylinder(counter.add_surface(), R = coeff)
+		else:
+			errstr = "'dim' must be 'xplane', 'yplane', 'zplane', or 'zcylinder'"
+			raise AssertionError(errstr)
+		surfdict[key] = openmc_surf
+		return openmc_surf
 
 
