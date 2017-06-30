9ee9de71ec37c3dfd9c6ed42e0488d92dbd5a5c8
tjlaboss <travis@ktc-technology.com>
2017-06-30 11:35:52 -0600

    Expanded the Mesh_Group class

--- pwr/meshes.py
+++ pwr/meshes.py
@@ -15,6 +15,8 @@ class Mesh_Group(object):
 	Parameters:
 	-----------
 	pitch:          float, or tuple of (xpitch, ypitch); cm
+	nx:             int; number of cuts in the x-direction
+	ny:             int; number of cuts in the y-direction
 	mesh_filter:    openmc.MeshFilter for ALL of the meshes
 	lower_left:     tuple of (x0, y0, z0); the lower left coordinate (cm)
 					of all the meshes
@@ -24,19 +26,19 @@ class Mesh_Group(object):
 					[Default: 1]
 	"""
 	
-	def __init__(self, pitch, mesh_filter, lower_left = (0, 0, 0), id0 = 1):
+	def __init__(self, pitch, nx, ny, mesh_filter, lower_left = (0, 0, 0), id0 = 1):
 		if isinstance(pitch, (int, float)):
 			self.xpitch, ypitch = pitch, pitch
 		elif len(pitch) in (2, 3):
 			self.xpitch, ypitch = pitch[0:2]
 		else:
 			raise IndexError("`pitch` must be of length 1, 2, or 3")
-		
+		self._nx = nx
+		self._ny = ny
 		self.mesh_filter = mesh_filter
 		self._meshes = []
 		self._mesh_edges = None
-		self.lower_left = lower_left
-		self.z0 = lower_left[2]
+		self.x0, self.y0, self.z0 = lower_left
 		self._z = self.z0
 		self._id0 = id0
 	
@@ -49,9 +51,16 @@ class Mesh_Group(object):
 		return self._id0
 	
 	@property
+	def nx(self):
+		return self._nx
+	
+	@property
+	def ny(self):
+		return self._ny
+	
+	@property
 	def mesh_edges(self):
 		return self._mesh_edges
-	
 	
 	def add_mesh(self, z1 = None, nz = None, dz = None):
 		"""Add a mesh to the group. You must supply two of the
@@ -72,11 +81,14 @@ class Mesh_Group(object):
 		elif dz and (z1 is not None):
 			nz = z1/dz
 			print(nz, int(nz))
-			# TODO: floor division or rounding?
+		# TODO: floor division or rounding?
 		
-		new_mesh = openmc.Mesh()
+		new_mesh = openmc.Mesh(self.id0)
+		new_mesh.type = "regular"
+		new_mesh.lower_left = (self.x0, self.y0, self._z)
+		new_mesh.width = (self._nx, self._ny, nz)
 		
-		
+		self._meshes.append(new_mesh)
+		self._id0 += 1
 		self._z = z1
-		
-	
+
