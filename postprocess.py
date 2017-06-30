bf081bc2a2cb02e3713c411800e8b9a9483314b9
tjlaboss <travis@ktc-technology.com>
2017-06-30 16:44:18 -0600

    Pretty plot for 3A

--- postprocess.py
+++ postprocess.py
@@ -6,12 +6,12 @@ import openmc
 from matplotlib import pyplot
 from numpy import *
 
-STATEPOINT = "test1/statepoint.0110.h5"
+STATEPOINT = "test1/statepoint.0100.h5"
 sp = openmc.StatePoint(STATEPOINT)
 
 
 def process_fission_rate_tally(state, tally_name = "fission tally", lat_shape = (17, 17),
-                               case_name = ""):
+	                           case_name = ""):
 	"""Write and view the fission rate tally
 	
 	Input:
@@ -19,7 +19,8 @@ def process_fission_rate_tally(state, tally_name = "fission tally", lat_shape = 
 	"""
 	
 	# Get the
-	tally = state.get_tally(name = tally_name)
+	#tally = state.get_tally(name = tally_name)
+	tally = state.get_tally(id = 1)
 	tal_vals = tally.get_values(scores = ["fission"])
 	fission_rates = tal_vals[:, 0, 0]
 	
@@ -31,11 +32,60 @@ def process_fission_rate_tally(state, tally_name = "fission tally", lat_shape = 
 	
 	# Plot
 	fig = pyplot.figure()
-	pyplot.imshow(fission_rates.squeeze(), interpolation = 'none', cmap = 'jet')
+	hotplot = pyplot.imshow(fission_rates.squeeze(), interpolation = 'none', cmap = 'jet')
+	print(nanmax(fission_rates), nanmin(fission_rates))
+	pyplot.clim(nanmax(fission_rates), nanmin(fission_rates))
 	pyplot.title(case_name + " Fission Rates")
-	pyplot.colorbar()
+	pyplot.colorbar(hotplot)
 	return None
 
 
-process_fission_rate_tally(sp)
+#def assembly_fission_rate_tally(state, tally_name = "fission tally"):
+#	return None
+
+def axial_power_tally(state):
+	#print(state.tallies)
+	
+	# Get these from some external source
+	nzs = [1, 7, 1, 6, 1, 6, 1, 6, 1, 6, 1, 6, 1, 5]
+	dzs = [3.866, 8.2111429, 3.81, 8.065, 3.81, 8.065, 3.81, 8.065, 3.81, 8.065, 3.81, 8.065, 3.81, 7.9212]
+	z0 = 11.951
+	z = z0
+	zlist = zeros(sum(nzs))
+	xlist = zeros(sum(nzs))
+	
+	n = len(nzs)
+	k = 0
+	for i in range(n):
+		nz = nzs[i]
+		dz = dzs[i]
+		talvalsi = state.get_tally(id = i+1).get_values()
+		talvalsi.shape = (nz, 1)
+		for j in range(nz):
+			z += dz
+			zlist[k] = z
+			xlist[k] = talvalsi[j]/dz
+			k += 1
+	
+	xlist /= xlist.mean()
+	axial_plot(xlist, zlist)
+
+
+def axial_plot(xlist, zlist):
+	pyplot.figure()
+	pyplot.plot(xlist, zlist, "bs-", label = "Case 3A Axial")
+	pyplot.grid()
+	pyplot.xticks(linspace(0, 1.75, 8))
+	pyplot.yticks(linspace(0, 400, ))
+	pyplot.xlabel("Relative power")
+	pyplot.ylabel("z (cm)")
+	pyplot.xlim(0, 1.75)
+	pyplot.ylim(0, 400)
+	
+	pyplot.legend()
+	
+
+
+#process_fission_rate_tally(sp)
+axial_power_tally(sp)
 pyplot.show()
